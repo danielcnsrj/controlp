add049123935d1d7f9e39f834d7db84d0ae2d6ad
danielcnsrj <danielcnsrj@gmail.com>
2025-07-19 17:39:18 -0300

    commit inicial

---
+++ src/main/java/dev/danielcnsrj/controlp/ControlpApplication.java
@@ -0,0 +1,13 @@
+package dev.danielcnsrj.controlp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/danielcnsrj/controlp/ControlpApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.danielcnsrj.controlp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
