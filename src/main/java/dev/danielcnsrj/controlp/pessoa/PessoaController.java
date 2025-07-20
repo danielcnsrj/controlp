b700a164567a8aff515fd5adb52597f3e195b530
danielcnsrj <danielcnsrj@gmail.com>
2025-07-20 10:51:40 -0300

    Controller para pessoas

---
+++ src/main/java/dev/danielcnsrj/controlp/pessoa/PessoaController.java
@@ -0,0 +1,23 @@
+package dev.danielcnsrj.controlp.pessoa;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequestMapping("/pessoa")
+public class PessoaController {
+
+    @GetMapping
+    public String exibeFormularioDeCadastro() {
+        return "pessoa/formulario";
+    }
+
+    @PostMapping
+    public String cadastrarPessoa(DadosCadastroPessoa dados) {
+        System.out.println(dados);
+        return "/";
+    }
+    
+}
