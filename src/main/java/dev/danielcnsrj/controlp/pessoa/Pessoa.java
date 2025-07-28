9ef0831883f103fd2739f80a663828b8661694e9
danielcnsrj <danielcnsrj@gmail.com>
2025-07-28 11:36:08 -0300

    Configuração da entidade

--- src/main/java/dev/danielcnsrj/controlp/pessoa/Pessoa.java
+++ src/main/java/dev/danielcnsrj/controlp/pessoa/Pessoa.java
@@ -1,5 +1,10 @@
 package dev.danielcnsrj.controlp.pessoa;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,12 +12,17 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "pessoas")
 public class Pessoa {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
     private String cargo;
     private String codigo;
     private String nome;
 
     public static Pessoa from(DadosCadastroPessoa dados) {
-        return new Pessoa(dados.cargo(), dados.codigo(), dados.nome());
+        return new Pessoa();//dados.cargo(), dados.codigo(), dados.nome());
     }
 }
